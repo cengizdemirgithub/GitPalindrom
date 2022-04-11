5ced25008890090291b3c1444539a00058a403b3
cengizgithub <kontakt.cengiz.demir@gmail.com>
2022-04-11 23:57:57 +0200

    reverse

--- src/replit_package/TerstenYazdir.java
+++ src/replit_package/TerstenYazdir.java
@@ -1,2 +1,20 @@
-package replit_package;public class TerstenYazdir {
+package replit_package;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TerstenYazdir {
+    public static void main(String[] args) {
+        int input=1238;
+        String sayi=String.valueOf(input);
+        List<String> sayiList=new ArrayList(List.of(sayi.split("")));
+        System.out.print(input + " reverse = ");
+        for (int i = sayiList.size()-1; i >=0 ; i--) {
+            System.out.print(sayiList.get(i));
+
+        }
+
+
+
+    }
 }
