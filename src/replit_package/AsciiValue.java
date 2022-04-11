b4880af4ef129b9b9ec819ea803bbda38c4cff1c
cengizgithub <kontakt.cengiz.demir@gmail.com>
2022-04-11 23:36:53 +0200

    Ascii

---
+++ src/replit_package/AsciiValue.java
@@ -0,0 +1,9 @@
+package replit_package;
+
+public class AsciiValue {
+    public static void main(String[] args) {
+        char t;
+        for(t='a';t<='z';t++){
+            System.out.println("Ascii value of "+t+" = "+Integer.valueOf(t));}
+    }
+}
