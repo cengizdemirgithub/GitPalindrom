7f6b01d182636b09208cb8e119fe6cde80af5dff
cengizgithub <kontakt.cengiz.demir@gmail.com>
2022-04-12 12:08:13 +0200

    _

---
+++ src/replit_package/YüzeKadar.java
@@ -0,0 +1,13 @@
+package replit_package;
+
+public class YüzeKadar {
+    public static void main(String[] args) {
+
+            int toplam = 0;
+            for (int i = 1; i <= 100; i++) {
+                toplam += i;
+
+            }
+        System.out.println(toplam);
+
+        }}
