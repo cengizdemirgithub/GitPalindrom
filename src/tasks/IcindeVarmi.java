f9f41fb64377008376fc565737ade47634bcdf65
cengizgithub <kontakt.cengiz.demir@gmail.com>
2022-04-20 23:25:51 +0200

    20.04.2022

---
+++ src/tasks/IcindeVarmi.java
@@ -0,0 +1,40 @@
+package tasks;
+
+import java.util.Scanner;
+
+public class IcindeVarmi {
+    static boolean kontrol = false;
+    static String str;
+
+    public static void main(String[] args) {
+        /*  TASK :
+         * girilen String icinde "xyz" dizimi var ise true degilse false return eden method create ediniz.
+         *
+         * input      output
+         * axyzm  ==   true
+         * xyz    ==   true
+         * x.yz   ==   false
+         * xyaz   ==   false
+         */
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("birseyler yaziniz");
+
+        try {
+            str = scanner.next();
+
+        } catch (Exception StringIndexOutOfBoundsException) {
+
+
+        }
+        for (int i = 0; i < str.length(); i++) {
+
+            if (str.substring(i, i + 3).equals("xyz")) {
+                kontrol = true;
+                break;
+            }else break;
+
+        }
+        System.out.println("bu metin icinde \"xyz\" var midir? : " + kontrol);
+
+    }
+}
