2a490bb1d03748f96c7a7a8cfefeaded4aab825e
cengizgithub <kontakt.cengiz.demir@gmail.com>
2022-05-03 23:57:47 +0200

    03.05.2022

--- src/interviewQuestions/Frage_1.java
+++ src/interviewQuestions/Frage_1.java
@@ -2,12 +2,23 @@ package interviewQuestions;
 
 public class Frage_1 {
     public static void main(String[] args) {
-        int sayi1=10;
-        int sayi2=20;
+        int sayi1=15;
+        int sayi2=3;
         int temp=sayi1;
         sayi1=sayi2;
         sayi2=temp;
         System.out.println("sayi1: "+sayi1+"\nsayi2: "+sayi2);
+        method(sayi1,sayi2);
 
+    }
+
+    private static void method(int sayi1, int sayi2) {
+
+     sayi1=sayi2+sayi1;
+     sayi2=(sayi1-sayi2);
+     sayi1=sayi1-sayi2;
+
+
+        System.out.println("sayi1= "+sayi1+"\nsayi2="+sayi2);
     }
 }
