29426fdd75fac53263856d28d783efe66ab3be92
cengizgithub <kontakt.cengiz.demir@gmail.com>
2022-05-13 23:51:32 +0200

    13.05.2022

--- src/JavaÜbung/Array3.java
+++ src/JavaÜbung/Array3.java
@@ -1,9 +1,12 @@
 package JavaÜbung;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 public class Array3 {
+
     public static void main(String[] args) {
         // int array list oluşturun 10 elemandan oluşmalı
 
@@ -13,20 +16,33 @@ public class Array3 {
         // eğer çift sayı atarsa o elemanının yerine 111 yazsın
         // eğer çift sayı yoksa çift sayı yoktur mesajını versin.
 
+        int arr[] = {10};
+        randomAdd(arr);
+
 
     }
 
     private static void randomAdd(int[] arr) {
-        ArrayList<Integer> arrList=new ArrayList<>();
-        for (int i = 0; i <10 ; i++) {
+        List<Integer> arrList = new ArrayList<>(10);
+        List<Integer> arrIlkList = new ArrayList<>(10);
+        Random rand = new Random();
+        for (int i = 0; i < 11; i++) {
+            arrList.add(i, rand.nextInt(1, 20));
+            arrIlkList.add(arrList.get(i));
 
-            arrList.add((int) Math.random()*20);
-
+            if (arrList.get(i) % 2 == 0) {
+                arrList.remove(i);
+                arrList.add(i, 111);
             }
 
 
+        }
+        if (!arrList.contains(111)) {
+            System.out.println("dizide cift yoktur");
+        }
 
-        System.out.println(arrList);
+        System.out.println("ilk hali: " + arrIlkList);
+        System.out.println("son hali: " + arrList);
 
     }
 }
