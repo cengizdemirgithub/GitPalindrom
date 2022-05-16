dd785b222156bb0a0e093f914c4e1c54196b71d2
cengizgithub <kontakt.cengiz.demir@gmail.com>
2022-05-16 23:43:13 +0200

    16.05.2022

--- src/lambdaPractice/Lambda_Space.java
+++ src/lambdaPractice/Lambda_Space.java
@@ -1,13 +1,16 @@
 package lambdaPractice;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Lambda_Space {
     public static void main(String[] args) {
         List<Integer> yeni=new ArrayList<>();
         yeni.add(1);
+        yeni.add(13);
         yeni.add(2);
         yeni.add(3);
         yeni.add(-3);
@@ -22,7 +25,45 @@ public class Lambda_Space {
         System.out.println("pozitif(yeni) = " + pozitif(yeni));
         // S4: list in elemanlarin karelerinden yeni bir list olusturalim
         System.out.println("kare(yeni) = " + kare(yeni));
+        //S5 : list in elemanlarin karelerinden tekrarsiz yeni bir list olusturalim
+        System.out.println("kareTekrarsiz(yeni) = " + kareTekrarsiz(yeni));
+        //S6: listin elemanlarini kucukten buyuge siralayalim
+        kücüktenByeSirala(yeni);
+        System.out.println();
+        //S7: listin elemanlarini buyukten kucuge siralayalim
+        büyüktenKye(yeni);
+        // S8: list pozitif elemanlari icn kuplerini bulup birler basamagi 5 olanlardan yeni bir list olusturali
+        System.out.println();
+        System.out.println("küpBirler5(yeni) = " + küpBirler5(yeni));
+        System.out.println();
+        //S9: list pozitif elemanlari icn karelerini bulup birler basamagi 5 olmayanlardan yeni bir list olustur
+        System.out.println("karenBirNo5(yeni) = " + karenBirNo5(yeni));
 
+    }
+
+    private static List karenBirNo5(List<Integer> yeni) {
+        List<Integer> karenBirNo5=yeni.stream().filter(t->t>0).map(t->t*t).filter(t->t%10!=0).collect(Collectors.toList());
+        return karenBirNo5;
+    }
+
+    private static List küpBirler5(List<Integer> yeni) {
+        List<Double>küpBes=yeni.stream().filter(t->t>0).map(t->Math.pow(t,3)).filter(t->t%10==7).collect(Collectors.toList());
+        return küpBes;
+
+    }
+
+    private static void büyüktenKye(List<Integer> yeni) {
+        yeni.stream().sorted(Comparator.reverseOrder()).forEach(t-> System.out.print(t+" "));
+    }
+
+    private static void kücüktenByeSirala(List<Integer> yeni) {
+        yeni.stream().sorted().forEach(t-> System.out.print(t+" "));
+    }
+
+    private static Set<Integer> kareTekrarsiz(List<Integer> yeni) {
+        Set<Integer> kareTekrarsiz=yeni.stream().map(t->t*t).collect(Collectors.toSet());
+
+        return kareTekrarsiz;
     }
 
     private static List kare(List<Integer> yeni) {
